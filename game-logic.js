3b31e540a9a67da8ad645afb435de2676db79b24
Linden Kyaw <kyawlinden@gmail.com>
2023-09-17 16:44:18 -0700

    Add score and logic to determine the overall winner

--- game-logic.js
+++ game-logic.js
@@ -22,8 +22,12 @@ function getComputerChoice() {
   return choices[choice];
 }
 
+let humanScore = 0;
+let computerScore = 0;
+
 function playRound(playerSelection, computerSelection) {
   let isWinner = false;
+  let isTie = false;
   let resultMessage = `You ${isWinner ? "Win" : "Lose"}! ${
     isWinner ? playerSelection : computerSelection
   } beats ${isWinner ? computerSelection : playerSelection}`;
@@ -40,16 +44,36 @@ function playRound(playerSelection, computerSelection) {
       break;
   }
 
-  if (playerSelection === computerSelection) resultMessage = "Tie!";
+  if (playerSelection === computerSelection) {
+    resultMessage = "Tie!";
+    isTie = true;
+  }
+
+  if (isWinner) {
+    humanScore++;
+  } else if (!isTie) {
+    computerScore++;
+  }
 
   return resultMessage;
 }
 
 function game() {
-  const playerSelection = getPlayerChoice();
-  const computerSelection = getComputerChoice();
-  const result = playRound(playerSelection, computerSelection);
-  alert(result);
+  while (humanScore !== 5 && computerScore !== 5) {
+    let playerSelection = getPlayerChoice();
+    let computerSelection = getComputerChoice();
+    let immediateResult = playRound(playerSelection, computerSelection);
+    console.log(immediateResult);
+    console.log(`Human: ${humanScore} - Computer: ${computerScore}`);
+  }
+
+  if (humanScore > computerScore) {
+    console.log("Human Wins!");
+  } else if (humanScore < computerScore) {
+    console.log("Computer Wins!");
+  } else {
+    console.log("Tie!");
+  }
 }
 
 game();
