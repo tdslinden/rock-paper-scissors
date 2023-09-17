a148839d41f9ff2bc441b912c60ca45a2ce83124
Linden Kyaw <kyawlinden@gmail.com>
2023-09-17 16:31:59 -0700

    Fix logic to display the correct result message

--- game-logic.js
+++ game-logic.js
@@ -1,16 +1,16 @@
-const choices = ["rock", "paper", "scissors"];
+const choices = ["Rock", "Paper", "Scissors"];
 
 function getPlayerChoice() {
   console.log("Human vs. Computer: Rock, Paper, Scissors");
 
   let input = prompt("Hi human, type rock, paper, or scissors: ");
-  let formattedSelection = input.toLowerCase();
+  let formattedSelection = input.charAt(0).toUpperCase() + input.slice(1);
   let isValidChoice = choices.includes(formattedSelection);
 
   while (!isValidChoice) {
     console.log(`${input} is invalid`);
     input = prompt("Hi human, type rock, paper, or scissors: ");
-    formattedSelection = input.toLowerCase();
+    formattedSelection = input.charAt(0).toUpperCase() + input.slice(1);
     isValidChoice = choices.includes(formattedSelection);
   }
 
@@ -25,10 +25,8 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
   let isWinner = false;
   let resultMessage = `You ${isWinner ? "Win" : "Lose"}! ${
-    playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)
-  } beats ${
-    computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)
-  }`;
+    isWinner ? playerSelection : computerSelection
+  } beats ${isWinner ? computerSelection : playerSelection}`;
 
   switch (playerSelection) {
     case "rock":
@@ -50,7 +48,8 @@ function playRound(playerSelection, computerSelection) {
 function game() {
   const playerSelection = getPlayerChoice();
   const computerSelection = getComputerChoice();
-  playRound(playerSelection, computerSelection);
+  const result = playRound(playerSelection, computerSelection);
+  alert(result);
 }
 
 game();
